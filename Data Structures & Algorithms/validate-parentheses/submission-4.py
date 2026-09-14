bd3f6ae0b860a9dbc1d327c114e0073067913d17
Hrithik Vishwakarma <hrithik.hv10@gmail.com>
2026-09-14 23:26:48 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_stack = []
+        hash_map = {")": "(", "]": "[", "}": "{"}
+
+        for char in s:
+
+            if char in hash_map:
+                if not open_stack:
+                    return False
+                if hash_map[char] != open_stack.pop():
+                    return False 
+            else:
+
+                open_stack.append(char)
+
+        return not open_stack
+            
